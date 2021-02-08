c20c7f02c04d4dbc77061c838992fe2be1028cf3
JCS <joshsad2@gmail.com>
2021-02-08 14:14:45 -0800

    working on users and session controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::Base
+
+    helper_method :current_user, :logged_in?
+
+    def current_user
+        return nil unless session[:session_token]
+        @current_user ||= User.find_by(session_token: session[session_token])
+    end
+    
+    def require_logged_in
+        redirect_to new_session_url unless logged_in?
+    end 
+
+    def login
+    end
+
+    def logout
+    end
+
+    def logged_in?
+        !!current_user
+    end
+
 end
