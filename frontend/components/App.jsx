c6fe7d33b7d01d29a572fa12e06c302332860da1
JCS <joshsad2@gmail.com>
2021-02-10 22:33:26 -0800

    added a 404 component

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -4,15 +4,17 @@ import { Route, Switch } from 'react-router-dom'
 import NavBarContainer from './navbar/navbar_container'
 import LoginFormContainer from './session_form/login_form_container'
 import SignupFormContainer from './session_form/signup_form_container'
+import NotFoundPage from './notFound'
 
-import {AuthRoute} from '../util/route_util'
+import { AuthRoute } from '../util/route_util'
 
 const App = () => (
     <div>
-        <Route exact path="/" component={NavBarContainer} />
         <Switch>
+            <Route exact path="/" component={NavBarContainer} />
             <AuthRoute exact path="/login" component={LoginFormContainer} />
             <AuthRoute exact path="/signup" component={SignupFormContainer} />
+            <Route component={NotFoundPage} />
         </Switch>
     </div>
 )
---
+++ frontend/components/notFound.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import NavBarContainer from './navbar/navbar_container'
+
+const NotFoundPage = () => {
+    return (
+        <div>
+            <NavBarContainer />
+            <div className="warning-headline">
+                <div className="page-status">404 Error.</div>
+                <div className="page-message">Doggone it! The page you’re looking for cannot be found.</div>
+            </div>
+        </div>
+        
+    )
+}
+
+export default NotFoundPage
