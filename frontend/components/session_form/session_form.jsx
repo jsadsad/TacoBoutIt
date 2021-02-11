03c4b952384e9b1595acc1b40aa0aca0016beed4
JCS <joshsad2@gmail.com>
2021-02-10 20:49:18 -0800

    added styling to login

--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -32,26 +32,46 @@ class SessionForm extends React.Component {
     render() {
         if (this.props.formType === "login") {
             return (
-                <div className="session-form">
-                    <div className="signin-wrapper">
-                        <form onSubmit={this.handleSubmit}>
-                            <p>Sign In:</p>
-                            <div>
-                                <input type="text"
-                                    value={this.state.email}
-                                    onChange={this.handleField('email')}
-                                    placeholder="Email"
-                                />
-                                <input type="password"
-                                    value={this.state.password}
-                                    onChange={this.handleField('password')}
-                                    placeholder="Password"
-                                />
-                                <input className="click-login" type="submit" value={this.props.formType} />
-                            </div>
-                        </form>
+                <div>
+                    <div className="header">
+                        <Link className="title" to="/">TacoBoutIt</Link>
                     </div>
-                    
+                    <div className="session-form">
+                        <div className="sign-in-wrapper"> 
+                            <div className="sign-in-for">Log in to TacoBoutIt</div>
+                            <div className="new-signin">New To TacoBoutIt? <span><Link className="signup-link" to="/signup">Sign up!</Link></span></div>
+                            <div className="terms">By logging in, you agree to TacoBoutIt's Terms of Service and Privacy Policy.</div>
+                            <div className="demo" onClick={this.handleClick}>Demo User</div>
+                            <div>
+                                <fieldset className="sep-line">
+                                    <legend align="center">
+                                        OR
+                                        </legend>
+                                </fieldset>
+                            </div>
+                            <form onSubmit={this.handleSubmit}>
+                                <div className="sign-in-form">
+                                    <div>
+                                        <input type="text"
+                                            value={this.state.email}
+                                            onChange={this.handleField('email')}
+                                            placeholder="Email"
+                                        />
+                                        <input type="password"
+                                            value={this.state.password}
+                                            onChange={this.handleField('password')}
+                                            placeholder="Password"
+                                        />
+                                        <button className="click-login" type="submit" value={this.props.formType}>{this.props.formType}</button>
+                                        <div className="bottom-new-sign-up">New To TacoBoutIt? <span><Link className="signup-link" to="/signup">Sign up!</Link></span></div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        <div className="classy-image">
+                            <img src={window.classyImage} alt="A Classy Image" />
+                        </div>
+                    </div>
                 </div>
             )
         } else if (this.props.formType === 'signup') {
@@ -62,12 +82,10 @@ class SessionForm extends React.Component {
                     </div>
                     <div className="session-form">
                         <form onSubmit={this.handleSubmit}>
-                            <div className="signup-wrapper">
-                                <h3 className="signup-for">Sign Up for TacoBoutIt</h3>
-                                <div className="signup-subhead">Connect with great local businesses</div>
-                                <div className="terms">
-                                    By continuing, you agree to TacoBoutIt's Terms of Service and acknowledge TacoBoutIt's Privacy Policy.
-                                </div>
+                            <div className="sign-up-wrapper">
+                                <h3 className="sign-up-for">Sign Up for TacoBoutIt</h3>
+                                <div className="sign-up-subhead">Connect with great local businesses</div>
+                                <div className="terms">By continuing, you agree to TacoBoutIt's Terms of Service and acknowledge TacoBoutIt's Privacy Policy.</div>
                                 <div className="demo" onClick={this.handleClick}>Demo User</div>
                                 <div>
                                     <fieldset className="sep-line">
@@ -117,7 +135,7 @@ class SessionForm extends React.Component {
                                             placeholder="Location"
                                         />
                                     </div>
-                                    <input className="click-signup" type="submit" value={this.props.formType} />
+                                    <button className="click-signup" type="submit" value={this.props.formType}>{this.props.formType}</button>
                                     <div className="already-exists">Already on TacoBoutIt? <Link to="/login">Log in</Link></div>
                                 </div>
                             </div>
