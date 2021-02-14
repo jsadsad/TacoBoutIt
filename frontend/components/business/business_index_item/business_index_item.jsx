465d1f17abecdb04cde351381a82563f80ddc82d
JCS <joshsad2@gmail.com>
2021-02-13 21:25:39 -0800

    styled index page

--- frontend/components/business/business_index_item/business_index_item.jsx
+++ frontend/components/business/business_index_item/business_index_item.jsx
@@ -1,24 +1,24 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faDollarSign } from '@fortawesome/free-solid-svg-icons'
+import { faDollarSign, faCheck, faTimes } from '@fortawesome/free-solid-svg-icons'
 
 const BusinessIndexItem = ({ business, index }) => (
     <div className="biz-index-search-result">
         <img src="https://via.placeholder.com/210" className="biz-image" alt="business_image" />
         <div className="biz-info">
-            <h2 className="biz-index-subtitle"><Link to={`/businesses/${business.id}`}>{index + 1}. {business.name}</Link></h2>
-            <p><FontAwesomeIcon icon={faDollarSign} size="xs" /><FontAwesomeIcon icon={faDollarSign} size="xs"/></p>
-            <span className="biz-category-tag">{business.category}</span>
+            <h2><Link className="biz-index-subtitle" to={`/businesses/${business.id}`}>{index + 1}. {business.name}</Link></h2>
+            <p><FontAwesomeIcon icon={faDollarSign} color="grey" size="xs" /><FontAwesomeIcon icon={faDollarSign} size="xs" color="grey"/><span className="biz-category-tag">{business.category}</span></p>
+            <div className="biz-outdoor-takeout">
+                <FontAwesomeIcon icon={faCheck} size="lg" color="green" fixedWidth/>Delivery
+                <FontAwesomeIcon icon={faCheck} size="lg" color="green" fixedWidth/>Takeout
+                <FontAwesomeIcon icon={faTimes} size="lg" color="red" fixedWidth />Outdoor Seating
+            </div>
         </div>
         <div className="biz-index-contact-info">
             <p>{business.phone_number}</p>
             <p>{business.address}</p>
-            <p>{business.city}</p>
-            <p>{business.state}</p>
-            <p>{business.zip_code}</p>
         </div>
-        <p className="biz-take-out">Takeout or Delivery?</p>
     </div>
 )
 
