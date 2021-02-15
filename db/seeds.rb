7cdce728f7cfaaa3f2a915b9bcab03c87fdd7e90
JCS <joshsad2@gmail.com>
2021-02-15 00:16:42 -0800

    added review seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,7 @@
 
 User.destroy_all
 Business.destroy_all
+Review.destroy_all
 
 ActiveRecord::Base.connection.tables.each do |t|
   ActiveRecord::Base.connection.reset_pk_sequence!(t)
@@ -20,3 +21,8 @@ User.create(:first_name => "Queue", :last_name => "Tea", :email => "matcha@boba.
 Business.create(:name => "House of Prime Rib", :website => "https://www.houseofprimerib.net/", :category => "American (Traditional)", :address => "1906 Van Ness Ave", :city => "San Francisco", :state => "CA", :zip_code => 94109, :phone_number => "415-885-4605", :lat => 37.7934405903263, :lng => -122.42273256793617)
 Business.create(:name => "Spoon House Bakery and Restaurant", :website => "http://places.singleplatform.com/spoon-house/menu?ref=google", :category => "Italian", :address => "1601 W Redondo Beach Blvd", :city => "Gardena", :state => "CA", :zip_code => 90247, :phone_number => "(310) 538-0376", :lat => 33.89007225223046, :lng => -118.3048875687615)
 Business.create(:name => "Thai Elephant Express", :website => "https://zmenu.com/thai-elephant-express-san-jose-online-menu/", :category => "Thai", :address => "3005 Silver Creek Rd Ste 128", :city => "San Jose", :state => "CA", :zip_code => 95121, :phone_number => "(408) 528-8882", :lat => 37.30874462435048, :lng => -121.81341205217721)
+
+Review.create(:content => "Our steak was too overcooked. It was hard to chew and I spit it out!", :rating => 2, :author_id => 2, :business_id => 1)
+Review.create(:content => "Pasta was super gooood! The place followed COVID-19 guidelines", :rating => 5, :author_id => 3, :business_id => 2)
+Review.create(:content => "Mmmmm...pad thai is always the right choice.", :rating => 4, :author_id => 1, :business_id => 3)
+Review.create(:content => "Don't trust this place! I got food poisoning the next day. Defintely the Pad Thai.", :rating => 1, :author_id => 3, :business_id => 3)
