5edf9297985e5db38a746241b05ee10c5be2ced7
JCS <joshsad2@gmail.com>
2021-02-16 12:58:23 -0800

    added reviewsReducer to entities

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -2,10 +2,12 @@ import { combineReducers } from "redux"
 
 import usersReducer from "./users_reducer"
 import businessesReducer from './businesses_reducer'
+import reviewsReducer from './reviews_reducer'
 
 const entitiesReducer = combineReducers({
     users: usersReducer,
-    businesses: businessesReducer
+    businesses: businessesReducer,
+    reviews: reviewsReducer
 })
 
 export default entitiesReducer
