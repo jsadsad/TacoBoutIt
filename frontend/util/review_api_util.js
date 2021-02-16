edacd780c4dd1a1e2b97be7bed60eddd85f37175
JCS <joshsad2@gmail.com>
2021-02-16 11:09:37 -0800

    created api util for reviews

---
+++ frontend/util/review_api_util.js
@@ -0,0 +1,29 @@
+export const fetchReviews = (businessId) => {
+    return $.ajax({
+        method: `GET`,
+        url: `/api/businesses/${businessId}/reviews`
+    })
+}
+
+export const createReview = (review) => {
+    return $.ajax({
+        method: `POST`,
+        url: `/api/businesses/api/businesses/${review.business_id}/reviews`,
+        data: { review }
+    })
+}
+
+export const updateReview = (review) => {
+    return $.ajax({
+        method: `PATCH`,
+        url: `/api/businesses/${review.business_id}/reviews/${review.id}`,
+        data: { review }
+    })
+}
+
+export const deleteReview = (review) => {
+    return $.ajax({
+        method: `DELETE`,
+        url: `/api/businesses/${review.business_id}/reviews/${review.id}`,
+    })
+}
