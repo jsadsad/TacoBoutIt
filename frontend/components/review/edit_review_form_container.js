79266406960129669fd3e7e15dee8f52896a9e2b
JCS <joshsad2@gmail.com>
2021-02-16 14:10:37 -0800

    added skeleton and container for ReviewForm

---
+++ frontend/components/review/edit_review_form_container.js
@@ -0,0 +1,18 @@
+import { connect } from 'react-redux'
+import { } from '../../actions/review_actions'
+import { } from '../../actions/business_actions'
+import ReviewForm from './review_form'
+
+const mapStateToProps = state => {
+    return {
+
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ReviewForm)
---
+++ frontend/components/review/review_form.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class ReviewForm extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div>This is the review form</div>
+        )
+    }
+}
+
+export default ReviewForm 
