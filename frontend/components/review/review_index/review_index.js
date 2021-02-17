3cc4102eb94867a1a5636c37f33d1e6fb82a176d
JCS <joshsad2@gmail.com>
2021-02-16 21:20:15 -0800

    created skeleton for review index and review item

---
+++ frontend/components/review/review_index/review_index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class ReviewIndex extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div>Review Index</div>
+        )
+    }
+}
+
+export default ReviewIndex
---
+++ frontend/components/review/review_index_item/review_index_item.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class ReviewIndexItem extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div>Review Index Item</div>
+        )
+    }
+}
+
+export default ReviewIndexItem
