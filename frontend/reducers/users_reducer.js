d44875d245cb3f17f05ea9b8cb08e9803c18878f
JCS <joshsad2@gmail.com>
2021-02-18 14:14:10 -0800

    added users index reducer

--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -1,12 +1,16 @@
+import { RECEIVE_USERS } from '../actions/user_actions'
 import { RECEIVE_CURRENT_USER } from '../actions/session_actions'
 
 const usersReducer = (oldState = {}, action) => {
   Object.freeze(oldState)
+  const nextState = Object.assign({}, oldState)
   switch (action.type) {
     case RECEIVE_CURRENT_USER:
       return Object.assign({}, oldState, {
         [action.currentUser.id]: action.currentUser,
       })
+    case RECEIVE_USERS:
+      return action.users
     default:
       return oldState
   }
