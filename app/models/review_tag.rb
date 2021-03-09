f1bd212cfaa0860f0a7629f21113ddb00a7c4231
JCS <joshsad2@gmail.com>
2021-03-09 15:43:20 -0800

    worked on review tag model

--- app/models/review_tag.rb
+++ app/models/review_tag.rb
@@ -1,2 +1,12 @@
-class ReviewTag < ApplicationRecord
+class Reaction < ApplicationRecord
+    validates :useful, :funny, :cool, inclusion: { in: [true, false] }
+
+    belongs_to :author,
+    foreign_key: :author_id,
+    class_name: :User
+
+    belongs_to :review,
+    foreign_key: :review_id,
+    class_name: :Review
+
 end
