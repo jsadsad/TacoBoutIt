c414b0c3258c145a20dcf07c142cf8859985e02e
JCS <joshsad2@gmail.com>
2021-03-09 14:20:09 -0800

    updated business index

--- frontend/components/business/business_index_item/business_index_item.jsx
+++ frontend/components/business/business_index_item/business_index_item.jsx
@@ -8,37 +8,51 @@ import {
 } from '@fortawesome/free-solid-svg-icons'
 
 const BusinessIndexItem = ({ business, index }) => (
-  <div className="biz-index-search-result">
-    <img
-      src={business.indexPicture}
-      className="biz-image"
-      alt="business_image"
-    />
+  <div className="biz-search-container">
+    <Link to={`/businesses/${business.id}`}>
+      <div className="biz-index-search-result">
+        <img
+          src={business.indexPicture}
+          className="biz-image"
+          alt="business_image"
+        />
 
-    <div className="biz-info">
-      <h2>
-        <Link className="biz-index-subtitle" to={`/businesses/${business.id}`}>
-          {index + 1}. {business.name}
-        </Link>
-      </h2>
-      <p>
-        <FontAwesomeIcon icon={faDollarSign} color="grey" size="xs" />
-        <FontAwesomeIcon icon={faDollarSign} size="xs" color="grey" />
-        <span className="biz-category-tag">{business.category}</span>
-      </p>
-      <div className="biz-outdoor-takeout">
-        <FontAwesomeIcon icon={faCheck} size="lg" color="green" fixedWidth />
-        Delivery
-        <FontAwesomeIcon icon={faCheck} size="lg" color="green" fixedWidth />
-        Takeout
-        <FontAwesomeIcon icon={faTimes} size="lg" color="red" fixedWidth />
-        Outdoor Seating
+        <div className="biz-info">
+          <h2>
+            <div className="biz-index-subtitle">
+              {index + 1}. {business.name}
+            </div>
+          </h2>
+          <p>
+            <FontAwesomeIcon icon={faDollarSign} color="grey" size="xs" />
+            <FontAwesomeIcon icon={faDollarSign} size="xs" color="grey" />
+            <span className="biz-category-tag">{business.category}</span>
+          </p>
+          <div className="biz-outdoor-takeout">
+            <FontAwesomeIcon
+              icon={faCheck}
+              size="lg"
+              color="green"
+              fixedWidth
+            />
+            Delivery
+            <FontAwesomeIcon
+              icon={faCheck}
+              size="lg"
+              color="green"
+              fixedWidth
+            />
+            Takeout
+            <FontAwesomeIcon icon={faTimes} size="lg" color="red" fixedWidth />
+            Outdoor Seating
+          </div>
+        </div>
+        <div className="biz-index-contact-info">
+          <p>{business.phoneNumber}</p>
+          <p>{business.address}</p>
+        </div>
       </div>
-    </div>
-    <div className="biz-index-contact-info">
-      <p>{business.phoneNumber}</p>
-      <p>{business.address}</p>
-    </div>
+    </Link>
   </div>
 )
 
