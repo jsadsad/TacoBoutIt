2bf3de01f70d1aec3ab1d21b36556cb68e7bda98
Joshua C. Sadsad <joshsad2@gmail.com>
2021-03-24 19:10:29 -0700

    updated landing suggestions

--- frontend/components/landing_page/landing_suggestions/landing_suggestions.js
+++ frontend/components/landing_page/landing_suggestions/landing_suggestions.js
@@ -4,30 +4,54 @@ import { Link } from 'react-router-dom'
 const LandingSuggestions = () => (
   <div>
     <div className="landing-suggestions-container">
-      <div className="landing-suggestion-title">Hot & New Businesses</div>
+      <div className="landing-suggestion-title">Explore New Businesses</div>
       <div className="landing-suggestion-index">
         <div className="landing-suggestion-category">
           <Link to="/businesses">
             <img src="https://tbi-seeds.s3-us-west-1.amazonaws.com/Restaurant_Splash.jpg"></img>
           </Link>
           <div>
-            <p className="landing-suggestions-caption">Resturants</p>
+            <p className="landing-suggestions-caption">Businesses</p>
           </div>
         </div>
         <div className="landing-suggestion-category">
-          <Link to="/businesses">
-            <img src="https://tbi-seeds.s3-us-west-1.amazonaws.com/Night_Life.jpeg"></img>
+          <Link to="/businesses/thai">
+            <img src="https://tbi-seeds.s3-us-west-1.amazonaws.com/splash/khai_soi.jpeg"></img>
           </Link>
           <div>
-            <p className="landing-suggestions-caption">Night Life</p>
+            <p className="landing-suggestions-caption">Thai</p>
           </div>
         </div>
         <div className="landing-suggestion-category">
-          <Link to="/businesses">
-            <img src="https://tbi-seeds.s3-us-west-1.amazonaws.com/Shopping.jpeg" />
+          <Link to="/businesses/japanese">
+            <img src="https://tbi-seeds.s3-us-west-1.amazonaws.com/splash/splash_japanese.jpeg" />
           </Link>
           <div>
-            <p className="landing-suggestions-caption">Shopping</p>
+            <p className="landing-suggestions-caption">Japanese</p>
+          </div>
+        </div>
+        <div className="landing-suggestion-category">
+          <Link to="/businesses/korean">
+            <img src="https://tbi-seeds.s3-us-west-1.amazonaws.com/splash/splash_korean.jpeg" />
+          </Link>
+          <div>
+            <p className="landing-suggestions-caption">Korean</p>
+          </div>
+        </div>
+        <div className="landing-suggestion-category">
+          <Link to="/businesses/chinese">
+            <img src="https://tbi-seeds.s3-us-west-1.amazonaws.com/splash/splash_chinese.jpeg" />
+          </Link>
+          <div>
+            <p className="landing-suggestions-caption">Chinese</p>
+          </div>
+        </div>
+        <div className="landing-suggestion-category">
+          <Link to="/businesses/tea">
+            <img src="https://tbi-seeds.s3-us-west-1.amazonaws.com/splash/splash_boba.jpeg" />
+          </Link>
+          <div>
+            <p className="landing-suggestions-caption">Tea</p>
           </div>
         </div>
       </div>
