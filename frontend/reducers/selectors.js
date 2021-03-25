4c920123e8f45ad181fe114d0ac552155aedeb2e
Joshua C. Sadsad <joshsad2@gmail.com>
2021-03-24 17:34:26 -0700

    working on business categories feature

--- frontend/reducers/selectors.js
+++ frontend/reducers/selectors.js
@@ -7,3 +7,45 @@ export const getReviewsForBusiness = (state, ownProps) => {
     (review) => review.businessId == ownProps.businessId
   )
 }
+
+export const getAmericanBiz = (state) => {
+  return Object.values(state.entities.businesses).filter(
+    (business) => business.category === 'American (Traditional)'
+  )
+}
+
+export const getItalianBiz = (state) => {
+  return Object.values(state.entities.businesses).filter(
+    (business) => business.category === 'Italian'
+  )
+}
+
+export const getThaiBiz = (state) => {
+  return Object.values(state.entities.businesses).filter(
+    (business) => business.category === 'Thai'
+  )
+}
+
+export const getJapaneseBiz = (state) => {
+  return Object.values(state.entities.businesses).filter(
+    (business) => business.category === 'Japanese'
+  )
+}
+
+export const getChineseBiz = (state) => {
+  return Object.values(state.entities.businesses).filter(
+    (business) => business.category === 'Chinese'
+  )
+}
+
+export const getKoreanBiz = (state) => {
+  return Object.values(state.entities.businesses).filter(
+    (business) => business.category === 'Korean'
+  )
+}
+
+export const getCofTeaBiz = (state) => {
+  return Object.values(state.entities.businesses).filter(
+    (business) => business.category === 'Coffee & Tea'
+  )
+}
